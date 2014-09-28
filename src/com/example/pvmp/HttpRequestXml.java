2a9047ff4085e056a690b6962b0f6c9d49fdfe86
David Carlos <ddavidcarlos1392@gmail.com>
2014-09-27 22:42:53 -0300

    Access xml node element values.

--- src/com/example/pvmp/HttpRequestXml.java
+++ src/com/example/pvmp/HttpRequestXml.java
@@ -10,11 +10,22 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
 import android.net.Uri;
 import android.os.AsyncTask;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Document;
 import android.util.Log;
 import android.util.Xml;
 
@@ -31,7 +42,7 @@ class HttpRequestXml {
 	}	
 	
 	
-	public static String loadData(String test_url) throws IOException, XmlPullParserException{
+	public static void loadData(String test_url) throws IOException, XmlPullParserException, ParserConfigurationException, SAXException{
 		
 		URL url = new URL(test_url);
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -43,18 +54,32 @@ class HttpRequestXml {
 		Log.d(null,"The response is:" + response);
 		
 		InputStream is = connection.getInputStream();
-		String name = parse(is);
-		Log.d("TESTE:" + name, "returning jsonList");
-		return name;
+		org.w3c.dom.Document name = parse(is);
+		org.w3c.dom.Element docEle = (org.w3c.dom.Element) name.getDocumentElement();
+		//org.w3c.dom.Element prop = (org.w3c.dom.Element) docEle.getElementsByTagName("proposicoes");
+		org.w3c.dom.NodeList nodeList = docEle.getChildNodes();
+		Log.d("SIZE:" + nodeList.getLength(),"node size");
+		for (int i = 0; i < nodeList.getLength(); i++){
+			//org.w3c.dom.Element el =  (org.w3c.dom.Element) nodeList.item(i);
+			//String id =  nodeList.item(i).getAttributes().getNamedItem("id").getTextContent();
+			if (nodeList.item(i).getNodeType() == Node.ELEMENT_NODE){
+				Element tmp = (Element) nodeList.item(i);
+				String id = tmp.getElementsByTagName("id").item(0).getTextContent();
+				Log.d("TESTE:" + id,"prop id");
+			}
+		}
 	}
 	
-	private static String parse (InputStream in) throws XmlPullParserException, IOException {
+	private static Document parse (InputStream in) throws XmlPullParserException, IOException, ParserConfigurationException, SAXException {
 		try{
-			XmlPullParser parser = Xml.newPullParser();
-			parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-			parser.setInput(in,null);
-			parser.nextTag();
-			return parser.getName();
+			//XmlPullParser parser = Xml.newPullParser();
+			//parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+			//parser.setInput(in,null);
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            org.w3c.dom.Document doc = db.parse(in);
+            org.w3c.dom.NodeList prop = doc.getElementsByTagName("proposições");
+			return doc;
 		}
 		finally{
 			in.close();
