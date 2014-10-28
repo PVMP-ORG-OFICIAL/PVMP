41e2dacbdbfbb7f597369c5041d44a0e52698fb6
David Carlos <ddavidcarlos1392@gmail.com>
2014-10-28 16:56:42 -0200

    Rename some  DatabaseInterface class atributes.

--- src/parser/DatabaseInterface.java
+++ src/parser/DatabaseInterface.java
@@ -23,7 +23,7 @@ public class DatabaseInterface extends SQLiteOpenHelper
 {
 
 	public static final String PROP_TABLE_NAME = "Proposition";
-	public static final String ID_PROP = "id";
+	public static final String ID_PROP = "id_prop";
 	public static final String ANO_PROP = "year";
 	public static final String EMENTA_PROP = "menu";
 	public static final String AUTHOR_PROP = "author";
@@ -46,53 +46,53 @@ public class DatabaseInterface extends SQLiteOpenHelper
 					+ ");";
 
 
-	public static final String VOTING_TABLE_NAME = "VOTING";
-	public static final String COD_SESSAO = "CODSESSAO";
-	public static final String RESUMO = "RESUMO";
-	public static final String DATA_VOTACAO = "DATA_VOTACAO";
+	public static final String VOTING_TABLE_NAME = "Voting";
+	public static final String COD_SESSAO = "code_session";
+	public static final String SUMMARY = "summary";
+	public static final String DATE = "date";
 
 	public static final String CREATE_VOTATING_TABLE = 
 			"CREATE TABLE " + VOTING_TABLE_NAME + "(" 
 					+ COD_SESSAO + " INTEGER NOT NULL PRIMARY KEY, " 
-					+ RESUMO + " TEXT, "
-					+ DATA_VOTACAO + " TEXT, "
+					+ SUMMARY + " TEXT, "
+					+ DATE + " TEXT, "
 					+ ID_PROP + " INTEGER, "
-					+ "FOREIGN KEY(IDPROP) REFERENCES PROPOSITION(IDPROP) "
+					+ "FOREIGN KEY(id_prop) REFERENCES Proposition(id_prop) "
 					+ ");";
 
 
 
-	public static final String PARTY_TABLE_NAME = "PARTY";
-	public static final String SIGLA = "SIGLA";
-	public static final String NUMERO = "NUMERO";
+	public static final String PARTY_TABLE_NAME = "Party";
+	public static final String ACRONYM_PARTY = "acronym";
+	public static final String NUMBER = "number_party";
 	public static final String CREATE_PARTY_TABLE = 
 			"CREATE TABLE " + PARTY_TABLE_NAME + "(" 
-					+ NUMERO + " INTEGER NOT NULL PRIMARY KEY, " 
-					+ SIGLA + " TEXT "
+					+ NUMBER + " INTEGER NOT NULL PRIMARY KEY, " 
+					+ ACRONYM_PARTY + " TEXT "
 					+ ");";
-	public static final String DEPUTY_TABLE_NAME = "DEPUTY";
-	public static final String NOME = "NOME";
-	public static final String UF = "UF";
-	public static final String IDCADASTRO = "IDCADASTRO";
-	public static final String NUMPARTIDO = "NUMPARTIDO";
+	public static final String DEPUTY_TABLE_NAME = "Deputy";
+	public static final String NOME = "name";
+	public static final String UF = "uf";
+	public static final String IDCADASTRO = "id_registration";
+	public static final String NUMPARTIDO = "number_party";
 	public static final String CREATE_DEPUTY_TABLE = 
 			"CREATE TABLE " + DEPUTY_TABLE_NAME + "(" 
 					+ IDCADASTRO + " INTEGER NOT NULL PRIMARY KEY, " 
 					+ UF + " TEXT, "
 					+ NOME + " TEXT, "
 					+ NUMPARTIDO + " INTEGER NOT NULL, "
-					+"FOREIGN KEY(NUMPARTIDO) REFERENCES PARTY(NUMERO) "
+					+"FOREIGN KEY(number_party) REFERENCES Party(number_party) "
 					+ ");";
 
-	public static final String VOTE_TABLE_NAME = "VOTE";
-	public static final String VOTO = "VOTO";
+	public static final String VOTE_TABLE_NAME = "Vote";
+	public static final String VOTO = "vote_result";
 	public static final String CREATE_VOTE_TABLE = 
 			"CREATE TABLE " + VOTE_TABLE_NAME + "(" 
 					+ VOTO + " TEXT, " 
 					+ COD_SESSAO + " INTEGER, "
 					+ IDCADASTRO + " INTEGER, "
-					+ "FOREIGN KEY(CODSESSAO) REFERENCES VOTING(CODSESSAO), "
-					+ "FOREIGN KEY(IDCADASTRO) REFERENCES DEPUTY(IDCADASTRO) "
+					+ "FOREIGN KEY(code_session) REFERENCES Voting(code_session), "
+					+ "FOREIGN KEY(id_registration) REFERENCES Deputy(id_registration) "
 					+ ");";
 
 
@@ -135,20 +135,20 @@ public class DatabaseInterface extends SQLiteOpenHelper
     ContentValues values = new ContentValues();
     for(int i = 0; i < propList.size(); i++) {
       Cursor c = db.rawQuery(
-          "SELECT * FROM PROPOSITION WHERE  IDPROP= ' "
+          "SELECT * FROM Proposition WHERE  id_prop= ' "
           + propList.get(i).getIdProp() + "'", null);
       Log.d("ENTROU2", "ENTROU2");
       if (c.getCount() == 0) {
         Log.d("ENTROU", "ENTROU");
-        values.put("IDPROP", propList.get(i).getIdProp());
-        values.put("ANOPROP", propList.get(i).getYearProp());
-        values.put("AUTORPROP", propList.get(i).getAuthorProp());
-        values.put("EMENTAPROP", propList.get(i).getMenuProp());
-        values.put("SIGLAPROP", propList.get(i).getAccProp());
-        values.put("NUMEROPROP", propList.get(i).getNumProp());
-        values.put("SITUACAOPROP", propList.get(i).getSituationProp());
+        values.put("id_prop", propList.get(i).getIdProp());
+        values.put("year", propList.get(i).getYearProp());
+        values.put("author", propList.get(i).getAuthorProp());
+        values.put("menu", propList.get(i).getMenuProp());
+        values.put("acronym", propList.get(i).getAccProp());
+        values.put("number", propList.get(i).getNumProp());
+        values.put("situation", propList.get(i).getSituationProp());
         values.put("Category", propList.get(i).getCategoryProp());
-        long log_res = db.insert("PROPOSITION", null, values);
+        long log_res = db.insert("Proposition", null, values);
         if (log_res != -1) {
           Log.d("prop ", "Proposição salva");
         } else {
@@ -168,18 +168,18 @@ public class DatabaseInterface extends SQLiteOpenHelper
 		ContentValues values = new ContentValues();
 		for (int i = 0; i < votList.size(); i++) {
 			Cursor c = db.rawQuery(
-					"SELECT * FROM VOTING WHERE CODSESSAO = ' "
+					"SELECT * FROM Voting WHERE code_session = ' "
 							+ votList.get(i).getCodSessionVot() + "'", null);
 			if (c.getCount() == 0) {
-				values.put("DATA_VOTACAO", votList.get(i).getDateVot());
-				values.put("RESUMO", votList.get(i).getSummaryVot());
-				values.put("CODSESSAO", votList.get(i).getCodSessionVot());
-				values.put("IDPROP", idProp);
+				values.put("date", votList.get(i).getDateVot());
+				values.put("summary", votList.get(i).getSummaryVot());
+				values.put("code_session", votList.get(i).getCodSessionVot());
+				values.put("id_prop", idProp);
 				long log_res = db.insert("VOTING", null, values);
 				if (log_res != -1) {
-					Log.d("prop ", "Votação salva");
+					Log.d("voting", "Votação salva");
 				} else {
-					Log.d("prop ", "Votação não salva");
+					Log.d("voting", "Votação não salva");
 				}
 			}
 		}
@@ -192,22 +192,22 @@ public class DatabaseInterface extends SQLiteOpenHelper
 		ContentValues values = new ContentValues();
 		for(int i = 0; i < deputy.size(); i++){
 			Cursor c = db.rawQuery(
-					"SELECT * FROM DEPUTY WHERE IDCADASTRO = ' "
+					"SELECT * FROM Deputy WHERE id_registration = ' "
 							+ deputy.get(i).getIdRegistrtion() + "'", null);
 			if (c.getCount() == 0) {
-				values.put("IDCADASTRO", deputy.get(i).getIdRegistrtion());
-				values.put("UF", deputy.get(i).getUf());
-				values.put("NOME", deputy.get(i).getName());
-				Cursor partyID = db.rawQuery("SELECT NUMERO FROM PARTY WHERE SIGLA = '" 
+				values.put("id_registration", deputy.get(i).getIdRegistrtion());
+				values.put("uf", deputy.get(i).getUf());
+				values.put("name", deputy.get(i).getName());
+				Cursor partyID = db.rawQuery("SELECT number_party FROM Party WHERE acronym = '" 
 						+ deputy.get(i).getPartyName() + "'",null);
 				if(partyID.moveToFirst()){
-					values.put("NUMPARTIDO", partyID.getInt(0));
+					values.put("number_party", partyID.getInt(0));
 				}
-				long log_res = db.insert("DEPUTY", null, values);
+				long log_res = db.insert("Deputy", null, values);
 				if (log_res != -1) {
-					Log.d("prop ", "Deputado salvo");
+					Log.d("deputy ", "Deputado salvo");
 				} else {
-					Log.d("prop ", "Deputado salvo");
+					Log.d("deputy ", "Deputado salvo");
 				}
 
 			}
@@ -223,14 +223,14 @@ public class DatabaseInterface extends SQLiteOpenHelper
 		SQLiteDatabase db = helper.getWritableDatabase();
 		ContentValues values = new ContentValues();
 		for(int i = 0; i < vote.size(); i++){
-			values.put("VOTO", vote.get(i).getResVote());
-			values.put("CODSESSAO", vote.get(i).getCodSession());
-			values.put("IDCADASTRO", deputy.get(i).getIdRegistrtion());
-			long log_res = db.insert("VOTE", null, values);
+			values.put("vote_result", vote.get(i).getResVote());
+			values.put("code_session", vote.get(i).getCodSession());
+			values.put("id_registration", deputy.get(i).getIdRegistrtion());
+			long log_res = db.insert("Vote", null, values);
 			if (log_res != -1) {
-				Log.d("Voto", "Voto salvo");
+				Log.d("Vote", "Voto salvo");
 			} else {
-				Log.d("prop ", "Voto não salvo");
+				Log.d("Vote ", "Voto não salvo");
 			}
 		}
 		db.close();
@@ -241,14 +241,14 @@ public class DatabaseInterface extends SQLiteOpenHelper
 		SQLiteDatabase db = helper.getWritableDatabase();
 		ContentValues values = new ContentValues();
 		Cursor c = db.rawQuery(
-				"SELECT * FROM PARTY WHERE NUMERO = ' "
+				"SELECT * FROM Party WHERE number_party = ' "
 						+ party.getNumParty() + "'", null);
 		if (c.getCount() == 0) {
-			values.put("NUMERO", party.getNumParty());
-			values.put("SIGLA", party.getAccParty());
-			long log_res = db.insert("PARTY", null, values);
+			values.put("number_party", party.getNumParty());
+			values.put("acronym", party.getAccParty());
+			long log_res = db.insert("Party", null, values);
 			if (log_res != -1) {
-				Log.d("prop ", "Partido salvo");
+				Log.d("party ", "Partido salvo");
 			} else {
 				Log.d("prop ", "Partido não salvo");
 			}
