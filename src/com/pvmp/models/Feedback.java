d4cc5b0f68c001d880e937a02a0264561b5baebe
Makrau <iago006@hotmail.com>
2014-11-13 15:18:20 -0200

    Add Feedback Model Class

---
+++ src/com/pvmp/models/Feedback.java
@@ -0,0 +1,78 @@
+package com.pvmp.models;
+
+import android.content.ContentValues;
+
+import com.pvmp.dao.DAOAbstract;
+
+public class Feedback extends DAOAbstract 
+{
+	// !--- DATABASE ATTRIBUTES ---! \\
+	public static final String COLUMN_USERNAME = "user_name";
+	public static final String COLUMN_PROPOSITION = "id_prop";
+	public static final String COLUMN_PARTY = "number_party";
+	public static final String COLUMN_OPINION = "opinion"; //Maybe change
+	
+	// !--- OTHER ATTRIBUTES ---! \\
+	private String opinion;
+	private User user;
+	private int target;
+	
+	public String getOpinion() 
+	{
+		return opinion;
+	}
+
+	public void setOpinion(String _opinion) 
+	{
+		this.opinion = _opinion;
+	}
+
+	public User getUser() 
+	{
+		return user;
+	}
+
+	public void setUser(User _user) {
+		this.user = _user;
+	}
+
+	public int getTarget() 
+	{
+		return target;
+	}
+
+	public void setTarget(int _target) 
+	{
+		this.target = _target;
+	}
+	
+
+	@Override
+	public ContentValues generateContentValues() 
+	{
+		ContentValues contentValues = new ContentValues();
+		/*(this.target instanceof Proposition)
+		{
+			Proposition proposition = new Proposition();
+			proposition = (Proposition) this.target;
+			
+		}*/
+		contentValues.put(COLUMN_PROPOSITION, this.target);
+		contentValues.put(COLUMN_USERNAME, this.user.getUsername());
+		contentValues.put(COLUMN_OPINION, this.opinion);
+		
+		return contentValues;
+	}
+
+	@Override
+	public DAOAbstract contentValuesToModel(ContentValues _contentValues) 
+	{
+		Feedback feedback = new Feedback ();
+		
+		feedback.setOpinion(_contentValues.getAsString(COLUMN_OPINION));
+		feedback.setTarget(_contentValues.getAsInteger(COLUMN_PROPOSITION));
+		
+		return feedback;
+	}
+	
+}
