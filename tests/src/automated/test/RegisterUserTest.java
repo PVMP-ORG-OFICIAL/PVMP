9d98def70697d56603d87cf72a1fc49ab0e4c0d4
Ana Carolina <ana_karolopes@hotmail.com>
2014-11-28 11:58:59 -0200

    Changes in automated test RegisterUser

--- tests/src/automated/test/RegisterUserTest.java
+++ tests/src/automated/test/RegisterUserTest.java
@@ -2,16 +2,17 @@ package automated.test;
 
 import com.android.uiautomator.core.UiObject;
 import com.android.uiautomator.core.UiObjectNotFoundException;
+import com.android.uiautomator.core.UiScrollable;
 import com.android.uiautomator.core.UiSelector;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
 public class RegisterUserTest extends UiAutomatorTestCase
 {
-	public void testRegister() throws UiObjectNotFoundException
+	public void testIfEmailIsValid() throws UiObjectNotFoundException
 	{
-	
-		UiObject pvmp = new UiObject(new UiSelector().text("PVMP"));
-		pvmp.clickAndWaitForNewWindow();
+		
+		UiObject donc = new UiObject(new UiSelector().text("De Olho na Câmara"));
+		donc.clickAndWaitForNewWindow();
 		
 		UiObject registerButton = new UiObject(new UiSelector().index(6).text("Cadastrar"));
 		registerButton.clickAndWaitForNewWindow();
@@ -21,9 +22,27 @@ public class RegisterUserTest extends UiAutomatorTestCase
 		name.click();
 		name.setText("ana");
 		
-		UiObject email = new UiObject(new UiSelector().index(4).text("Inserir o seu email"));
-		email.click();
-		email.setText("a@email.com");
+		UiObject invalidEmail = new UiObject(new UiSelector().index(4).text("Inserir o seu email"));
+		invalidEmail.click();
+		invalidEmail.setText("a@email");
+		
+		new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().index(13)
+				.text("Cadastrar").className("android.widget.Button"));
+		new UiObject( new UiSelector().index(13).text("Cadastrar")
+				.className("android.widget.Button")).click();
+		
+		UiObject registerButton1 = new UiObject(new UiSelector().index(13).text("Cadastrar"));
+		registerButton1.clickAndWaitForNewWindow();
+		sleep(200);
+		
+		UiObject validEmail = new UiObject(new UiSelector().index(4).text("Inserir o seu email"));
+		validEmail.click();
+		validEmail.setText("a@email.com");
+		
+	}
+	
+	public void testIfUsernameIsValid() throws UiObjectNotFoundException
+	{
 		
 		UiObject age = new UiObject(new UiSelector().index(6).text("Insira sua idade"));
 		age.click();
@@ -35,9 +54,14 @@ public class RegisterUserTest extends UiAutomatorTestCase
 		UiObject sex = new UiObject(new UiSelector().index(1).text("Feminino"));
 		sex.click();
 		
-		UiObject userName = new UiObject(new UiSelector().index(9).text("Nome de usuário"));
-		userName.click();
-		userName.setText("ana");
+		new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().index(9)
+				.text("Nome de usuário").className("android.widget.EditText"));
+		new UiObject( new UiSelector().index(9).text("Nome de usuário")
+				.className("android.widget.EditText")).click();
+		
+		UiObject invalidUsername = new UiObject(new UiSelector().index(9).text("Nome de usuário"));
+		invalidUsername.click();
+		invalidUsername.setText("00ana");
 		
 		UiObject password = new UiObject(new UiSelector().index(11));
 		password.click();
@@ -46,9 +70,50 @@ public class RegisterUserTest extends UiAutomatorTestCase
 		UiObject confirmPassword = new UiObject(new UiSelector().index(12));
 		confirmPassword.click();
 		confirmPassword.setText("ana123");
+	
+		UiObject confirmRegisterButton = new UiObject(new UiSelector().index(13).text("Cadastrar"));
+		confirmRegisterButton.clickAndWaitForNewWindow();
+		sleep(200);
 		
-		UiObject registerButton1 = new UiObject(new UiSelector().index(13).text("Cadastrar"));
-		registerButton1.clickAndWaitForNewWindow();
+		UiObject validUsername = new UiObject(new UiSelector().index(9).text("Nome de usuário"));
+		validUsername.click();
+		validUsername.setText("ana");
+	}
+	
+	public void testPasswordIsRight() throws UiObjectNotFoundException
+	{
+		
+		UiObject password = new UiObject(new UiSelector().index(11));
+		password.click();
+		password.setText("ana123");
+		
+		UiObject confirmPassword = new UiObject(new UiSelector().index(12));
+		confirmPassword.click();
+		confirmPassword.setText("ana12355");
+		
+		new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().index(13)
+				.text("Cadastrar").className("android.widget.Button"));
+		new UiObject( new UiSelector().index(13).text("Cadastrar")
+				.className("android.widget.Button")).click();
+		
+		UiObject confirmRegisterButton = new UiObject(new UiSelector().index(13).text("Cadastrar"));
+		confirmRegisterButton.clickAndWaitForNewWindow();
+		sleep(200);
+		
+		UiObject password1 = new UiObject(new UiSelector().index(11));
+		password1.click();
+		password1.setText("ana123");
+		
+		UiObject rightConfirmPassword = new UiObject(new UiSelector().index(12));
+		rightConfirmPassword.click();
+		rightConfirmPassword.setText("ana123");
+	
+	}
+	
+	public void testValidRegister() throws UiObjectNotFoundException
+	{
+		UiObject confirmRegisterButton = new UiObject(new UiSelector().index(13).text("Cadastrar"));
+		confirmRegisterButton.clickAndWaitForNewWindow();
 		sleep(200);
 		
 		UiObject image = new UiObject(new UiSelector().index(1));
@@ -56,6 +121,8 @@ public class RegisterUserTest extends UiAutomatorTestCase
 		
 		UiObject exitButton = new UiObject(new UiSelector().index(1).text("Sair"));
 		exitButton.clickAndWaitForNewWindow();
+		
+		getUiDevice().pressBack();
 		
 	}
 
